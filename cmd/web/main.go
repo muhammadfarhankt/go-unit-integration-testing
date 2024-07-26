e6f82378c372a111b9eb437b796c3619a5704ea6
Muhammad Farhan KT <farhanlatheefkt@gmail.com>
2024-07-26 10:50:30 +0530

    test push

---
+++ cmd/web/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"encoding/gob"
+	"flag"
+	"log"
+	"net/http"
+	"webapp/pkg/data"
+	"webapp/pkg/db"
+
+	"github.com/alexedwards/scs/v2"
+)
+
+type application struct {
+	DSN     string
+	DB      db.PostgresConn
+	Session *scs.SessionManager
+}
+
+func main() {
+	gob.Register(data.User{})
+
+	// set up an app config
+	app := application{}
+
+	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=postgres password=postgres dbname=users sslmode=disable timezone=UTC connect_timeout=5", "Posgtres connection")
+	flag.Parse()
+
+	conn, err := app.connectToDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	app.DB = db.PostgresConn{DB: conn}
+
+	// get a session manager
+	app.Session = getSession()
+
+	// print out a message
+	log.Println("Starting server on port 8080...")
+
+	// start the server
+	err = http.ListenAndServe(":8080", app.routes())
+	if err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ primeNumberCheck/go.mod
@@ -0,0 +1,3 @@
+module primeCheck
+
+go 1.21.5
